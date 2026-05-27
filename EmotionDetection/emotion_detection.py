3c016874f71048f3bd7bd540cb308f592e394db8
jjrenteria <jjrenteria@gmail.com>
2026-05-26 23:29:41 -0600

    Update emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,11 @@ def emotion_detector(text_to_analyze):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
+    if response.status_code >= 400 and response.status_code < 500:
+        return "Cliente error"
+    if response.status_code >= 500 :
+        return "Server error"
+        
     res = response.json()['emotionPredictions']
     dictresponse =  res[0]['emotion']
     
