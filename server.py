f4b732a7c138dea64df58708d05d3f3699b3dee2
jjrenteria <jjrenteria@gmail.com>
2026-05-27 15:19:53 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,5 @@
+"""Web application for sentiment analysis"""
+
 from flask import Flask, render_template, request
 from EmotionDetection import emotion_detector
 
@@ -5,13 +7,24 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def send_analizer():
-    text_to_analize = request.args.get('textToAnalize')
+    """Perform a sentiment analysis of the input text"""
+    text_to_analize = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analize)
-    txt = f"""For the given statement, the system response is 'anger': {response['anger']}, 
-    'disgoust': {response['disgoust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}
-    The dominant emotion is {response['dominant_emotion']}."""   
-    return text
 
+    if response['dominant_emotion'] is None:
+        txt = "Invalid text! Please try again!."
+        return txt
+
+# pylint: disable=trailing-whitespace
+    txt = f"""For the given statement, the system response is 'anger': {response['anger']}, 
+    'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} and 'sadness': {response['sadness']}
+    The dominant emotion is {response['dominant_emotion']}."""   
+    return txt
+
+@app.route("/")
+def to_index():
+    """render index.html"""
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host="localhost", port=5000)
